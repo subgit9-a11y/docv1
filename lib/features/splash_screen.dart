fd230be1521acd616cc0974bb34d843e14726ae2
Subhash <subhash@example.com>
2026-08-03 22:10:22 +0530

    Restore prefConstatnt import needed for Preferences class in splash screen

--- lib/features/splash_screen.dart
+++ lib/features/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:doctro/core/constants/prefConstatnt.dart';
 import 'package:doctro/core/constants/preferences.dart';
 import 'package:doctro/core/navigator_key.dart';
 import 'package:doctro/theme/ayureze_theme.dart';
